bda436f847e0b20619a0beac84706da00612afe5
Yami Thalyssa <thalyssa2.0@hotmail.com>
2018-11-28 02:31:08 -0300

    Update user.java

--- user.java
+++ user.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class user {
 
@@ -11,8 +12,27 @@ public class user {
     public int communities; //IDs das comunidades que o usuário participa
     public ArrayList<String> messages = new ArrayList();
 
-    public String getFriendshipRequests(){
-        return request.toString();
+    public void getFriendshipRequests(){
+        int option;
+        Scanner key = new Scanner(System.in);
+
+        for(int i=0;i<request.size();i++) {
+            System.out.println("O usuário " + request.get(i).username + " deseja ser seu amigo");
+            System.out.println("Digite 1 para aceitar ou 2 para recusar");
+
+            option = key.nextInt();
+
+            switch (option) {
+                case 1:
+                    //Colocar os usuários um na lista de amigos do outro;
+                    break;
+                case 2:
+                    break;
+            }//Fim switch
+
+            //Excluir o usuário da lista de request
+
+        }//Fim do for
     }
 
     public user(int id, String login, String password, String username){
