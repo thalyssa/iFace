2de3be5be0276be6e950d0e6e1da7916575abac8
Yami Thalyssa <thalyssa2.0@hotmail.com>
2018-12-11 11:06:28 -0300

    Update main.java

--- main.java
+++ main.java
@@ -1,370 +1,135 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 import static java.lang.System.exit;
 
 public class main {
 
-    //RELATIVO AO USUÁRIO
-    public static user[] users = new user[1000];
-    public static int countUsers = 0;
-    public static int userActualID; //Id do usuário que está com a sessão aberta
+    public static int countEmployees = 0;
 
-    //RELATIVO ÀS COMUNIDADES
-    public static community[] communities = new community[1000];
-    public static int countCom = 0; //Quantidade de comunidades existentes;
+    public static ArrayList<employee> EMPLOYEES = new ArrayList<>();
 
-    //Cria uma conta de usuario no iFace;
-    public static void createUser(){
-        Scanner board = new Scanner(System.in);
-        String login;
-        String password;
-        String username;
+    public static void addEmployee(){
+        employee newEmployee;
+        String name, adress;
+        int type;
 
-        if(countUsers >= 1000){
-            System.out.println("Máximo de usuários!");
-        }else{
-            System.out.println("CRIAÇÃO DE USUÁRIO");
-            System.out.println("Por favor, digite um e-mail: ");
-            login = board.nextLine();
-            System.out.println("Agora digite uma senha: ");
-            password = board.nextLine();
-            System.out.println("Por último, digite um nome de usuário: ");
-            username = board.nextLine();
+        Scanner keyboard = new Scanner(System.in);
 
-            users[countUsers] = new user(countUsers, login, password, username);
-            countUsers++;
+        System.out.println("---CADASTRO DE FUNCIONÁRIO---\n");
+        System.out.println("Nome: ");
+        name = keyboard.nextLine();
+
+        System.out.println("Endereço: ");
+        adress = keyboard.nextLine();
+
+        System.out.println("Tipo de salário:\n1 - Horista\n2 - Mensalista\n3 - Comissionado");
+        type = keyboard.nextInt();
+
+        if(type<1 || type>3){
+            System.out.println("TIPO DE SALÁRIO INVÁLIDO. Por favor, tente novamente");
+            exit(0);
         }
+
+        newEmployee = new employee(countEmployees, name, adress, type);
+
+        EMPLOYEES.add(newEmployee);
+
+        countEmployees++;
     }
 
-    //Loga com um usuário no iFace e exibe a interface inicial
-    public static void signIn(){
-        Scanner board = new Scanner(System.in);
-        String login;
-        String password;
-
-        System.out.println("TELA DE LOGIN");
-        System.out.println("E-mail: ");
-        login = board.nextLine();
-        System.out.println("Senha: ");
-        password = board.nextLine();
-
-        for(int i=0;i<countUsers;i++){
-            if(login.equals(users[i].login)){
-                if(!password.equals(users[i].password)){
-                    System.out.println("Senha incorreta");
-                }else {
-                    //Exibe a tela de login e armazena o ID do usuário para usar nas operações como: Envio de mensagens, edição de perfil, etc.
-                    userActualID = users[i].id;
-                    System.out.println("\n");
-                    userLoggedScreen();
-                    return;
-                }
-            }
-        }//Fim do for
-
-        System.out.println("ERRO! Usuário não encontrado\nPor favor, verifique suas informações");
-        exit(0);
-    }
-
-    //Edita o perfil de um usuário
-    public static void editProfile(){
-        int option;
-        String data;
-        Scanner board = new Scanner(System.in);
-
-        System.out.println("EDIÇÃO DE PERFIL");
-        System.out.println("Dados atuais");
-        System.out.println("Login: " + users[userActualID].login);
-        System.out.println("Senha: " + users[userActualID].password);
-        System.out.println("Usuário: " + users[userActualID].username);
-        System.out.println("Favor, escolha o campo que deseja alterar: ");
-        System.out.println("1 - Login\n2 - Senha\n3 - Usuário");
-
-        option = board.nextInt();
-
-        switch(option){
-            case 1:
-                System.out.println("Digite o novo login: ");
-                data = board.next();
-                users[userActualID].login = data;
-                System.out.println("Login alterado com sucesso!");
-                break;
-            case 2:
-                System.out.println("Digite a nova senha: ");
-                data = board.next();
-                users[userActualID].password = data;
-                System.out.println("Senha alterada com sucesso!");
-                break;
-            case 3:
-                System.out.println("Digite o novo usuário: ");
-                data = board.next();
-                users[userActualID].username = data;
-                System.out.println("Usuário alterado com sucesso!");
-                break;
-            default:
-                System.out.println("Opção inválida!");
-                break;
-        }
-    }
-
-    //Deleta o perfil de um usuário
-    public static void deleteProfile(){
-        //DELETANDO AS INFORMAÇÕES DO PERFIL
-        users[userActualID].id = -1;
-
-        //DELETANDO AS COMUNIDADES EXISTENTES OERTENCENTES AO USUÁRIO
-        for(int i=0;i<countCom;i++){
-            if(communities[i].owner == userActualID){
-                communities[i].id = -1;
+    public static void deleteEmployee(int code){
+        for(int i=0;i<countEmployees;i++){
+            if(EMPLOYEES.get(i).getCode()== code){
+                EMPLOYEES.remove(EMPLOYEES.get(i));
             }
         }
-
-        System.out.println("Perfil deletado com sucesso!");
     }
 
-    //Retorna o ID do usuário
-    //PRONTA - TESTADA
-    public static int getUserID(String login){
-        for(int i=0;i<countUsers;i++){
-            if(login.equals(users[i].login) && users[i].id!=-1) {
-                return users[i].id;
+    public static int getEmployeeID(String name){
+        for(int i=0;i<countEmployees;i++){
+            if(name.equals(EMPLOYEES.get(i).getName()) && EMPLOYEES.get(i).getCode()!=-1) {
+                return EMPLOYEES.get(i).getCode();
             }
         }//Fim for
         return -1;
-    }//Fim getUserID
-
-    /*Envia um pedido de amizade registrando o usuário remetente na lista de solicitações pendentes do destinatário
-    Se o usuário destinatário recusar o pedido, o remetente é removido da sua lista*/
-    public static void sendFriendshipRqst(){
-        Scanner board = new Scanner(System.in);
-        String login;
-        int id;
-
-        System.out.println("Digite o login do seu amigo: ");
-        login = board.nextLine();
-
-        id = getUserID(login);
-
-        if(id == -1){
-            System.out.println("Usuário não encontrado!");
-            return;
-        }
-
-        users[id].request.add(users[userActualID]);
-        System.out.println("Pedido enviado");
     }
 
-    //Verificar pedidos de amizade
-    public static void seeFrendshipRqst(){
+    public static void update(){
 
-        int option;
-        int aux = users[userActualID].request.size();
         Scanner keyboard = new Scanner(System.in);
 
-        if(aux>0){
-            users[userActualID].getFriendshipRequests();
-        }else{
-            System.out.println("Você não tem novos pedidos de amizade");
-        }
+        String nome;
 
-    }
+        System.out.println("Digite o nome do funcionário: ");
+        nome = keyboard.nextLine();
 
-    //Criar uma comunidade e atribuir um dono
-    public static void createCommunity(){
-        Scanner board = new Scanner(System.in);
-        String cName;
-        String description;
+        int code = getEmployeeID(nome);
 
-        System.out.println("CRIAÇÃO DE COMUNIDADE");
-        System.out.println("Digite um nome para a comunidade: ");
-        cName = board.nextLine();
+        String name, adress;
+        int type, option;
 
-        System.out.println("Digite uma descrição para a comunidade: ");
-        description = board.nextLine();
-
-        communities[countCom] = new community(countCom, userActualID, cName, description);
-        countCom++;
-    }
-
-    //Exibe todas as comunidades existentes
-    public static void seeCommunities(){
-        for(int i=0;i<=countCom;i++){
-            System.out.println(communities[i].name);
-            System.out.println("Dono: " + users[communities[i].id].username);
-            System.out.println(communities[i].description);
-            System.out.println("ID de ingresso: " + communities[i].id + "\n");        
-            communities[i].getMembersList();
-        }
-    }
-
-    //Recuperar as informações do perfil
-    public static void searchProfile(){
-        System.out.println("--PERFIL--");
-        System.out.println("Nome: " + users[userActualID].username);
-        System.out.println("Login: " + users[userActualID].login);
-        System.out.println("\n");
-
-        //Exibe a lista de amigos
-        users[userActualID].getFriendsList();
-        System.out.println("\n");
-
-        //Exibe as mensagens
-        getMessages();
-
-        System.out.println("\n--COMUNIDADES--");
-        for(int i=0;i<=countCom;i++){
-            if(communities[i].owner == userActualID){
-                System.out.println("Nome: " + communities[i].name);
-            }
-        }
-    }
-
-    //Interface para o usuário logado
-    public static void userLoggedScreen(){
-        Scanner keyboard = new Scanner(System.in);
-        int option;
-
-        System.out.println("Bem vindo ao iFace, " + users[userActualID].username + "!\n Escolha o que deseja fazer: ");
-
-        System.out.println("1 - Editar perfil");
-        System.out.println("2 - Adicionar amigo");
-        System.out.println("3 - Ver pedidos de amizade");
-        System.out.println("4 - Criar comunidade");
-        System.out.println("5 - Ver comunidades");
-        System.out.println("6 - Se juntar a uma comunidade");
-        System.out.println("7 - Verificar caixa de mensagens");
-        System.out.println("8 - Enviar uma mensagem");
-        System.out.println("9 - Verificar informações do perfil");
-        System.out.println("10 - Deletar conta");
-        System.out.println("11 - Sair");
+        System.out.println("---MUDANÇA DE REGISTRO---\n");
+        System.out.println("Diga o que deseja alterar:\n1 - Nome\n2 - Endereço\n3 - Tipo de salário");
 
         option = keyboard.nextInt();
 
         switch (option){
             case 1:
-                editProfile();
+                System.out.println("Digite o novo nome: ");
+                name = keyboard.nextLine();
+                EMPLOYEES.get(code).setName(name);
                 break;
             case 2:
-                sendFriendshipRqst();
+                System.out.println("Digite o novo endereço: ");
+                adress = keyboard.nextLine();
+                EMPLOYEES.get(code).setAdreess(adress);
                 break;
+
             case 3:
-                seeFrendshipRqst();
+                System.out.println("Digite o novo tipo de salário:\n1 - Horista\n2 - Mensalista\n3 - Comissionado");
+                type = keyboard.nextInt();
+
+                if(type<1 || type>3){
+                    System.out.println("TIPO DE SALÁRIO INVÁLIDO. Por favor, tente novamente");
+                    exit(0);
+                }
+                EMPLOYEES.get(code).setType(code);
                 break;
-            case 4:
-               createCommunity();
-                break;
-            case 5:
-                seeCommunities();
-                break;
-            case 6:
-                joinCommunity();
-                break;
-            case 7:
-                getMessages();
-                break;
-            case 8:
-                sendMessage();
-                break;
-            case 9:
-                searchProfile();
-                break;
-            case 10:
-                deleteProfile();
-                break;
-            case 11:
-                return;
             default:
-                System.out.println("Opção inválida!");
+                System.out.println("Opção inválida");
                 break;
-
-        }
-
+        }//End switch
     }
 
-    //Enviar uma mensagem para outro usuário
-    public static void sendMessage(){
-        String message;
-        String sendTo;
-        int id;
+    public static void searchEmployee(){
+        String nome;
         Scanner keyboard = new Scanner(System.in);
 
-        System.out.println("Digite o destinatário: ");
-        sendTo = keyboard.nextLine();
+        System.out.println("Digite o nome do funcionário: ");
+        nome = keyboard.nextLine();
 
-        id = getUserID(sendTo);
+        int code = getEmployeeID(nome);
 
-        if(id == -1){
-            System.out.println("Usuário não encontrado!");
-        }else{
-            System.out.println("Digite a mensagem a ser enviada: ");
-            message = keyboard.nextLine() + "\nENVIADA POR: " + users[userActualID].username;
-
-            users[id].messages.add(message);
+        for(int i=0;i<countEmployees;i++){
+            if((EMPLOYEES.get(i).getCode()) == code);
+            System.out.println("--FUNCIONÁRIO--");
+            System.out.println("Nome: " + EMPLOYEES.get(i).getName());
+            System.out.println("Login: " + EMPLOYEES.get(i).getAdress());
+            System.out.println("\n");
         }
 
-    }
-    
-    //Permite ao membro se juntar a uma comunidade
-    public static void joinCommunity(){
-        Scanner keyboard = new Scanner(System.in);
-        int id;
-
-        System.out.println("Digite o ID da comunidade");
-        id = keyboard.nextInt();
-
-        communities[id].addMember(users[userActualID]);
-        
-        System.out.println("Você agora é um membro desta comunidade!");
+        System.out.println("--FUNCIONÁRIO NÃO ENCONTRADO--");
 
     }
 
-    //Exibe as mensagens do usuário
-    public static void getMessages(){
-        int aux = users[userActualID].messages.size();
-        System.out.println("--CAIXA DE MENSAGENS--");
-        for(int i=0;i<aux;i++){
-            System.out.println(users[userActualID].messages.get(i)+"\n");
-        }
-
-    }
-
-    //Texto da tela inicial
-    public static void initialScreen(){
-        System.out.println("\n\n\n");
-        System.out.println("Bem vindo ao iFace!\nPor favor, escolha uma opção: ");
-        System.out.println("1. Registrar-se");
-        System.out.println("2. Login");
-        System.out.println("3. Sair");
-    }
 
     public static void main(String args[]){
-
-        Scanner keyboard = new Scanner(System.in);
-        int option;
-
-        initialScreen();
-
-        option = keyboard.nextInt();
-
-        while(option!=3){
-            switch (option){
-                case 1:
-                    createUser();
-                    System.out.println("\n");
-                    initialScreen();
-                    break;
-                case 2:
-                    signIn();
-                    initialScreen();
-                    break;
-                case 3:
-                    exit(0);
-                    break;
-                default:
-                    System.out.println("Opção inválida!\nFavor, tente novamente");
-                    break;
-            }
-            option = keyboard.nextInt();
-        }
+        addEmployee();
+        searchEmployee();
+        update();
+        searchEmployee();
+        deleteEmployee(0);
+        searchEmployee();
 
     }
 }
